c271221115b68ebbc4714b837c7d40fa4ce9ca49
parwatpandit <parbatp777@gmail.com>
2026-04-19 10:11:15 +0100

    Phase 6: Rate limiting added

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Request
 from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine
 from routers import auth, users, transactions
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 import json
 
 Base.metadata.create_all(bind=engine)
 
+limiter = Limiter(key_func=get_remote_address)
+
 app = FastAPI(title="SentinelLedger", version="2.0")
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,7 +31,8 @@ app.include_router(users.router)
 app.include_router(transactions.router)
 
 @app.get("/")
-def root():
+@limiter.limit("5/minute")
+def root(request: Request):
     return {"message": "SentinelLedger v2 is running!"}
 
 @app.websocket("/ws/{account_number}")
